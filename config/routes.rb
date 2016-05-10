35872be57cc1cadc83daa28d2746ed0a2ea24ebf
Kelton Manzanares <keltonmanzanares@gmail.com>
2016-05-10 11:31:10 -0700

    user signup for auth completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
-
+  root            'static_pages#home'
   get 'signup' => 'users#new'
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
