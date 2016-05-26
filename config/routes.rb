8499aea7790fbcc1b899269890e8e784cf1dd297
Kelton Manzanares <keltonmanzanares@gmail.com>
2016-05-25 19:14:09 -0500

    starting on some cosmetic changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root               'static_pages#home'
+  root               'sessions#new'
   get    'signup' => 'users#new'
   get    'login'  => 'sessions#new'
   post   'login'  => 'sessions#create'
